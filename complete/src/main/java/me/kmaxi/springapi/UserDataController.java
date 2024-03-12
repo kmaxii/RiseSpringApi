ed85a0b95be3cb80f5b3b3d89ea80bb4c75c16e9
kmaxii <esst936@gmail.com>
2024-03-12 14:29:38 +0100

    Added integration with database

--- complete/src/main/java/me/kmaxi/springapi/UserDataController.java
+++ complete/src/main/java/me/kmaxi/springapi/UserDataController.java
@@ -1,5 +1,6 @@
 package me.kmaxi.springapi;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,11 +9,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserDataController {
 
+    @Autowired
+    private UserDataRepository userDataRepository;
+
     @PostMapping("/processUserData")
     public ResponseEntity<String> processUserData(@RequestBody UserDataListWrapper userDataListWrapper) {
-        // Example of processing: Loop through each UserData object and print details
+
+        int userId = (int) (Math.random() * Integer.MAX_VALUE);
+
         for (UserData userData : userDataListWrapper.getUserDataList()) {
-            // Add
+            UserDataEntity userDataEntity = new UserDataEntity();
+            userDataEntity.setUserID(userId);
+            userDataEntity.setTime(userData.getTime());
+            userDataEntity.setLongitude(userData.getLongitude());
+            userDataEntity.setLatitude(userData.getLatitude());
+            userDataEntity.setBusTransport(userData.getBusTransport());
+            userDataEntity.setActivityChange(userData.getActivityChange());
+            userDataEntity.setMoney(userData.getMoney());
+            userDataEntity.setOnScooter(userData.isOnScooter());
+            userDataEntity.setActivities(userData.getActivities());
+
+            userDataRepository.save(userDataEntity);
         }
         return ResponseEntity.ok("Processed userDataList successfully");
     }
---
+++ complete/src/main/java/me/kmaxi/springapi/UserDataEntity.java
@@ -0,0 +1,102 @@
+package me.kmaxi.springapi;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class UserDataEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int entryID;
+    private int userID;
+    private String time;
+    private double longitude;
+    private double latitude;
+    private String busTransport;
+    private String activityChange;
+    private float money;
+    private boolean isOnScooter;
+    private String activities;
+
+    public void setEntryID(int entryID) {
+        this.entryID = entryID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public void setBusTransport(String busTransport) {
+        this.busTransport = busTransport;
+    }
+
+    public void setActivityChange(String activityChange) {
+        this.activityChange = activityChange;
+    }
+
+    public void setMoney(float money) {
+        this.money = money;
+    }
+
+    public void setOnScooter(boolean onScooter) {
+        isOnScooter = onScooter;
+    }
+
+    public void setActivities(String activities) {
+        this.activities = activities;
+    }
+
+    public int getEntryID() {
+        return entryID;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public String getBusTransport() {
+        return busTransport;
+    }
+
+    public String getActivityChange() {
+        return activityChange;
+    }
+
+    public float getMoney() {
+        return money;
+    }
+
+    public boolean isOnScooter() {
+        return isOnScooter;
+    }
+
+    public String getActivities() {
+        return activities;
+    }
+}
