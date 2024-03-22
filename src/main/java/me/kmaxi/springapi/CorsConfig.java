967e27defb2b65352f8f007b01c41a49a10406dd
kmaxii <esst936@gmail.com>
2024-03-22 09:51:44 +0100

    Added cors mappings

---
+++ src/main/java/me/kmaxi/springapi/CorsConfig.java
@@ -0,0 +1,17 @@
+package me.kmaxi.springapi;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8000")
+                .allowedMethods("GET", "POSgT", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
