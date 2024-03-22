40aa3358f9c68acc26342467dcf925e9a8fee735
kmaxii <esst936@gmail.com>
2024-03-22 10:31:10 +0100

    updateed allowedorigins to allowedoriginpatterns

--- src/main/java/me/kmaxi/springapi/CorsConfig.java
+++ src/main/java/me/kmaxi/springapi/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*") // Allow requests from all origins
+                .allowedOriginPatterns("*") // Allow requests from all origins
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
